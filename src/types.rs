a11e5ca987344024ab646b8cf7e5417f80939fa5
Chris <rolletchris1@gmail.com>
2025-07-01 22:15:32 -0700

    simplified the structure types for segmentation

--- src/types.rs
+++ src/types.rs
@@ -1,15 +1,10 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct WordInfo {
+pub struct Segment {
     pub start: f32,
     pub end: f32,
     pub word: String,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Segment {
-    pub words: Vec<WordInfo>,
 }
 
 #[derive(Debug, Deserialize)]
