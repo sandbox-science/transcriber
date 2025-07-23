c49bf5766123e1bdae151f7b3f6f5a0bf93b3aef
Chris <rolletchris1@gmail.com>
2025-07-22 21:54:03 -0700

    ran cargo update

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,14 @@
 use clap::Parser;
 
 #[derive(Parser, Debug)]
-#[command(version = "1.0", author = "Chris Dedman")]
+#[command(
+    name = "Capski",
+    version = "0.1.0",
+    author = "Chris Dedman",
+    about = "Create karaoke-style videos from audio or video",
+    disable_help_flag = false,
+    disable_version_flag = false
+)]
 pub struct Opts {
     #[arg(short, long, required = true)]
     pub input: String,
